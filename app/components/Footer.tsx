78d6fb1091a4230c015f2def7d5f190d4b23c0ee
sherlyastha <asthapandeycse9@gmail.com>
2023-05-14 03:52:18 +0530

    fix-header

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (  
         <div className="text-sm text-white flex flex-row gap-1
-        fixed bottom-0 left-0 pb-4 pl-4
+         bottom-0 left-0 pb-4 pl-4
         ">
             <p>&copy; Astha Pandey </p>
             <p>  2023</p>
--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
       <>
-        <title>Linktree</title>
+        <title>Linktree by Astha</title>
         <meta name="description" content="Next.js + Tailwind CSS" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
